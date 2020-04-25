cf855d8c445d46e39770faafa02c14fc255ca336
ZhangZibo <zibo.zhang.byron@gmail.com>
2020-04-25 23:59:28 +0800

    Updates

---
+++ precache-manifest.e96f2f6e48111e4e24f752aee00dc021.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95fee9ec3565131035c73b2828e08d89",
+    "url": "/ReactApp_MovieZorro/index.html"
+  },
+  {
+    "revision": "b1ca070976bce762db20",
+    "url": "/ReactApp_MovieZorro/static/css/main.abba6d43.chunk.css"
+  },
+  {
+    "revision": "b304cc64b628c2e08934",
+    "url": "/ReactApp_MovieZorro/static/js/2.5bfd37c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactApp_MovieZorro/static/js/2.5bfd37c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1ca070976bce762db20",
+    "url": "/ReactApp_MovieZorro/static/js/main.09a37d7c.chunk.js"
+  },
+  {
+    "revision": "a6b88f64e4f35c5f5f39",
+    "url": "/ReactApp_MovieZorro/static/js/runtime-main.14112aef.js"
+  }
+]);
